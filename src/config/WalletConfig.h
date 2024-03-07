00d0dae727bce6b3cbd00f7e48e0194af31626ce
g0dspeak <letsmbpk9@gmail.com>
2024-03-06 23:17:41 -0700

    d# Changes not staged for commit:

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = xcrt;
+    const std::string_view addressPrefix = 0x784b4;
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "XCRT";
