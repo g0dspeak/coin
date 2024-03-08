3b7f55383c4601bdc07ece04bd56c09f3c1f205d
g0dspeak <letsmbpk9@gmail.com>
2024-03-08 15:02:52 -0700

    e

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -232,8 +232,9 @@ namespace CryptoNote
         static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
         /* Block heights we are going to have hard forks at */
-        const uint64_t FORK_HEIGHTS[] = { 
-           0,
+        const uint64_t FORK_HEIGHTS[] = 
+        { 
+           1200,
             };
         
 
